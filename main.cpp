df89b2631921a0a62cd4e8d076e88449f6c81eb7
진현우 <49802982+maizer2@users.noreply.github.com>
2021-08-18 23:50:29 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+void arr_parc() {
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int (*parr)[3] = arr;
+
+    printf("%p\n", arr);
+    printf("%d\n", sizeof(arr));
+    printf("%d\n", sizeof(arr[1]));
+
+}
+
 void basci_prac() {
     int a = 11;
     int b = 10;
@@ -29,7 +39,8 @@ void pointer_prac() {
 int main() {
 
     //pointer_prac();
-    basci_prac();
+    //basci_prac();
+    arr_parc();
     return 0;
 }
 
