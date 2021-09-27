e5eb18f1ca69972c2424034f03f5a71c306767c8
maizer2 <jhw3979@gmail.com>
2021-09-27 11:09:22 +0900

    rm project

--- cmake-build-debug/CMakeFiles/3.20.2/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/1. Develope/1. Language/1. C/4. Compiler/mingw64/bin/windres.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .obj)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
