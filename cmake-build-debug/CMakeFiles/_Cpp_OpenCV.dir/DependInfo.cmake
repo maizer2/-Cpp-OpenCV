5cd01b716f0fac9829a4acd75c4f60cb4c5f0177
maizer2 <jhw3979@gmail.com>
2021-10-14 16:00:49 +0900

    CMakeList 수정

---
+++ cmake-build-debug/CMakeFiles/_Cpp_OpenCV.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/4. Github/2. C++/-Cpp-OpenCV/main.cpp" "C:/4. Github/2. C++/-Cpp-OpenCV/cmake-build-debug/CMakeFiles/_Cpp_OpenCV.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/1. Develope/C/2. C++/opencv/build/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
-#include "opencv2/opencv.hpp"
 
-
-int main()
-{
-	std::cout << "Hello OpenCV " << CV_VERSION << std::endl;
-	
-	cv::Mat img;
-	img = cv::imread("lenna.bmp");
-
-	if (img.empty()) {
-		std::cerr << "Image load failed" << std::endl;
-		return -1;
-	}
-
-	cv::namedWindow("image");
-	cv::imshow("image", img);
-
-	cv::waitKey();
-	return 0;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
