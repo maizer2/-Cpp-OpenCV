af54dd8891d096792ed5ea65215198f4e7dd635c
maizer2 <jhw3979@gmail.com>
2021-10-18 20:23:40 +0900

    change IDE

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include "opencv2/opencv.hpp"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	std::cout << "Hello OpenCV" << CV_VERSION << std::endl;
+	
+	cv::Mat img;
+	img = cv::imread("lenna.bmp");
+
+	if (img.empty()) {
+		std::cerr << "Image load failed! " << std::endl;
+		return -1;
+	}
+
+	cv::namedWindow("lenna");
+	cv::imshow("lenna", img);
+
+	cv::waitKey();
+	return 0;
 }
